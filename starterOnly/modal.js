21a245d820016df36faf8c806a8f26488d8ccb15
QuentinGalloisDev <quentingallois@yahoo.com>
2023-07-03 15:16:43 +0200

    error messages & close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,5 +19,28 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// Close modal event
+const fermer = document.querySelector(".close");
+const contenu = document.querySelector(".content");
 
+fermer.addEventListener("click", closeModal);
+//J'anime la fermeture du formulaire avec animate 
+function closeModal() {
+  // contenu.animate([
+  //   {
+  //     opacity: "1",
+  //   },
 
+  //   {
+  //     transform: "translateY(150px)",
+  //     opacity: "0"
+  //   }
+  // ], {
+  //   duration: 950,
+  //   iterations: 1
+
+  // })
+  contenu.style.animation = "modalclose 1000ms ease"
+  setTimeout(function () { modalbg.style.display = "none"; }, 950);
+  setTimeout(function () { contenu.style.animation = ""; }, 950);
+}
